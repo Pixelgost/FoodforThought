21c6edf916c2baeba7738e04bbca8a7c9c8e5de5
Pixelgost <thisisadhi@gmail.com>
2023-01-20 21:45:00 -0500

    migrate to firebase

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const {google} = require('google')
-const blogger = google.blogger({
-    version: 'v3',
-    auth: 'AIzaSyC-eRqGispj_wUHIr55Xeult6POacDFpRs'
-  });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-eRqGispj_wUHIr55Xeult6POacDFpRs",
+  authDomain: "foodforthought-122112.firebaseapp.com",
+  projectId: "foodforthought-122112",
+  storageBucket: "foodforthought-122112.appspot.com",
+  messagingSenderId: "445971372513",
+  appId: "1:445971372513:web:64adc1aa0e1c4453f0daa0",
+  measurementId: "G-8L26H4EYQ4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
