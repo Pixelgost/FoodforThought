b62340405afefc10157d07aef581cfa66673c0e3
victor <victorgao0308@gmail.com>
2023-01-22 02:34:51 -0500

    added text box and buttons to request/donate goods

--- src/components/GiveOrReceiveGoods.js
+++ src/components/GiveOrReceiveGoods.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import NavBar from './NavBar';
-  
+import { TextField } from '@material-ui/core';
+import styled from 'styled-components';
+
+
+const ButtonGroup = styled.div`display: flex;`;
+const Button = styled.button`display: flex`;
 function GiveOrReceive() {
   return (
     <><h1>
           Give Or Receive Goods
       </h1><><div>
+        <div>
+          <h3>
+            Type the goods that you wish to give or receive in the box, along with their quantities, then click on the appropriate button.
+          </h3>
+        </div>
+        <div>
+        <TextField id="entry-box" 
+        label="" 
+        multiline
+        variant="outlined"
+        fullWidth = "fullWidth" />
+      
+      
+      
+        <ButtonGroup>
+        <Button> I want to receive these goods </Button>
+        <Button> I want to donate these goods</Button>
+        </ButtonGroup>
+
+        
+  
+
+        </div>
       </div><NavBar />
           </></>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,6 +6,21 @@ function Profile() {
     <><h1>
           Profile
       </h1><><div>
+        Here, you can change your display name, the name that other users can see.
+      <div>
+        You can also view your reputation, or star rating. You earn reputation by successfully completing donations.
+        <div>
+        </div>
+        The more donations you do and the more consistent you are, the more reputation you earn.
+        <div>
+        Note that if your reputation drops too low, your account may be suspended.
+        </div>
+
+        <h3>
+          Your current reputation is: 0.0
+        </h3>
+
+      </div>
       </div><NavBar />
           </></>
   );
